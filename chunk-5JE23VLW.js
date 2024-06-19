bca916aa6d78071599d599e020e94b768f20f8a9
Denis Micheletti <denis.micheletti@gmail.com>
2024-06-19 10:00:48 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/DenMic/MMAEagles/commit/659837621dcf86feb8f1369b334939619d53634a

---
+++ chunk-5JE23VLW.js
@@ -0,0 +1,1 @@
+import{Ja as i,R as o,Ua as a}from"./chunk-M3L2JH7M.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-kids"]],standalone:!0,features:[a],decls:1,vars:0,consts:[[1,"p-8","2xl:mx-64","xl:mx-24","flex","flex-col","gap-8"]],template:function(n,r){n&1&&i(0,"div",0)}});let t=e;return t})();export{p as KidsComponent};
